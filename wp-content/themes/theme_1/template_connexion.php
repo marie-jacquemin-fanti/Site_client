d859727ec6afb280fb32296fbd6825d5f4a3d804
MarieJF <marie.jacquemin-fanti@student.hepl.be>
2026-05-18 13:49:07 +0200

    Make the connexion pages

--- wp-content/themes/theme_1/template_connexion.php
+++ wp-content/themes/theme_1/template_connexion.php
@@ -1,1 +1,39 @@
-<?php /* Template Name: Connexion */ ?>
+<?php /* Template Name: Demande d'accès */ ?>
+<?php get_header() ?>
+
+<?php
+$deconnexion_button = get_field('deconnexion_button');
+
+$identification_acces = get_field('identification_acces');
+$scolaire_mail = get_field('scolaire_mail');
+?>
+
+<a class="buttons" href="<?= $deconnexion_button['url'] ?>"><?= $deconnexion_button['title'] ?></a>
+
+<p>Les enseignants ou membre de l'équipe PLAI peuvent ce connecter ici. Si c'est la premier fois une vérification sera
+    effectuer avant de pouvoir y accéder.</p>
+
+<form class="form" action="<?= admin_url('admin-post.php'); ?>" method="POST">
+    <div class="form__group">
+        <label class="form__label" for="fase">Numéro de fase d'implentation ⓘ *</label>
+        <input class="form__input" type="number" id="fase" name="fase" value="" placeholder="Ex : 5988" required/>
+    </div>
+
+    <div class="form__group">
+        <label class="form__label" for="email">Adresse mail de contact ⓘ *</label>
+        <input class="form__input" type="email" id="email" name="email" value="" placeholder="Ex : jeandumont@heple.be"
+               required/>
+    </div>
+
+    <div class="form__group">
+        <label class="form__label" for="message">Pourquoi je demande l'accée ? ⓘ *</label>
+        <textarea class="form__input" id="message" name="message" placeholder="Message..." required></textarea>
+    </div>
+    <?php if ($identification_acces): ?>
+    <a class="buttons" href="<?= $identification_acces['url'] ?>"><?= $identification_acces['title'] ?></a>
+    <?php endif; ?>
+    <?php if ($scolaire_mail): ?>
+    <a class="buttons" href="<?= $scolaire_mail['url'] ?>"><?= $scolaire_mail['title'] ?></a>
+    <?php endif; ?>
+</form>
+<?php get_footer() ?>
--- wp-content/themes/theme_1/template_identification.php
+++ wp-content/themes/theme_1/template_identification.php
@@ -1,44 +1,38 @@
 <?php /* Template Name: Identification */ ?>
+<?php get_header() ?>
+<?php
+$deconnexion_button = get_field('deconnexion_button');
+
+$identification = get_field('identification');
+$whitout_scolaire_mail = get_field('whitout_scolaire_mail');
+$fase_number_identification = get_field('fase_number_identification');
+$scolaire_mail = get_field('scolaire_mail');
+?>
+<a class="buttons" href="<?= $deconnexion_button['url'] ?>"><?= $deconnexion_button['title'] ?></a>
+
+<p>Les enseignants ou membre de l'équipe PLAI peuvent ce connecter ici. Si c'est la premier fois une vérification sera
+    effectuer avant de pouvoir y accéder.</p>
+
 <form class="form" action="<?= admin_url('admin-post.php'); ?>" method="POST">
     <div class="form__group">
-        <label class="form__label" for="name">Nom de famille *</label>
-        <input class="form__input" type="text" id="name" name="name" value="" placeholder="John Doe" required/>
-            <p class="form__error-message"><?= $errors['name']; ?></p>
+        <?php if ($fase_number_identification): ?>
+            <label class="form__label" for="fase"><?= $fase_number_identification ?></label>
+        <?php endif; ?>
+
+        <input class="form__input" type="number" id="fase" name="fase" value="" placeholder="Ex : 5988" required/>
     </div>
 
     <div class="form__group">
-        <label class="form__label" for="email">Adresse mail *</label>
-        <input class="form__input" type="email" id="email" name="email" value="" placeholder="Ex: johndoe@example.com" required/>
-        <?php if ($errors['email'] ?? null): ?>
-            <p class="form__error-message"><?= $errors['email']; ?></p>
+        <?php if ($fase_number_identification): ?>
+            <label class="form__label" for="email"><?= $scolaire_mail ?></label>
         <?php endif; ?>
+        <input class="form__input" type="email" id="email" name="email" value="" placeholder="Ex : jeandumont@heple.be"
+               required/>
     </div>
-
-    <div class="form__group">
-        <label class="form__label" for="phone">Numéro de téléphone *</label>
-        <input class="form__input" type="tel" id="phone" name="phone" value="" placeholder="Ex: +33 6 12 48 75 95" required/>
-        <?php if ($errors['phone'] ?? null): ?>
-            <p class="form__error-message"><?= $errors['phone']; ?></p>
-        <?php endif; ?>
-    </div>
-
-    <div class="form__group">
-        <label class="form__label" for="sujet">Sujet</label>
-        <input class="form__input" type="text" id="sujet" name="sujet" value="" placeholder="Ex: Prise de rendez-vous"/>
-        <?php if ($errors['sujet'] ?? null): ?>
-            <p class="form__error-message"><?= $errors['sujet']; ?></p>
-        <?php endif; ?>
-    </div>
-
-    <div class="form__group">
-        <label class="form__label" for="message">Message *</label>
-        <textarea class="form__textarea" id="message" name="message" placeholder="Ecrivez votre message ici..." required></textarea>
-        <?php if ($errors['message'] ?? null): ?>
-            <p class="form__error-message"><?= $errors['message']; ?></p>
-        <?php endif; ?>
-    </div>
-
-    <input type="hidden" name="action" value="contact_form"/>
-    <input type="hidden" name="contact_nonce" value="<?= wp_create_nonce('contact_form'); ?>"/>
-    <button class="buttons" type="submit">Envoyer</button>
+    <?php if ($identification): ?>
+        <a class="buttons" href="<?= $identification['url'] ?>"><?= $identification['title'] ?></a>
+    <?php endif; ?>
+    <?php if ($whitout_scolaire_mail): ?>
+        <a class="buttons" href="<?= $whitout_scolaire_mail['url'] ?>"><?= $whitout_scolaire_mail['title'] ?></a>
+    <?php endif; ?>
 </form>
